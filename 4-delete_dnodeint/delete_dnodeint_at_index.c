2791482238d52ef324608851191f4f1039edba6b
Azeez Adebayo <debaycisse@gmail.com>
2024-03-24 11:31:41 +0100

    correct the logical error
    correct the logical error to ensure that
    the removing of a node is done correctly.

--- 4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -43,7 +43,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		(*head)->prev = (*head)->next;
+		(*head)->prev->next = (*head)->next;
 		if ((*head)->next != NULL)
 		        (*head)->next->prev = (*head)->prev;
 		free(*head);
